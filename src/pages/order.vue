d03e0ae4849c09685c83e830e657422b6b00c8b6
zhangzhulei <zhangzhulei@100tal.com>
2020-10-27 17:57:51 +0800

    初始化项目

---
+++ src/pages/order.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav-header />
+    <router-view />
+    <nav-footer />
+  </div>
+</template>
+
+<script>
+  import NavHeader from "@/components/NavHeader";
+  import NavFooter from "@/components/NavFooter";
+  export default {
+    name: "order",
+    components: {NavFooter, NavHeader}
+  }
+</script>
+
+<style scoped>
+
+</style>
