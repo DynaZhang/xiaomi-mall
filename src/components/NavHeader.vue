38acb012b6ff05b3656267a23f1d26bd2ee7e547
zhangzhulei <zhangzhulei@100tal.com>
2020-10-29 17:21:39 +0800

    临时提交

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -115,79 +115,79 @@
               </ul>
             </div>
           </div>
-<!--          <div class="item-menu">-->
-<!--            <span>电视</span>-->
-<!--            <div class="children">-->
-<!--              <ul>-->
-<!--                <li class="product">-->
-<!--                  <a href="javascript:void(0)" target="_blank">-->
-<!--                    <div class="pro-img">-->
-<!--                      <img-->
-<!--                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"-->
-<!--                        alt="小米10至尊纪念版" />-->
-<!--                    </div>-->
-<!--                    <div class="pro-name">小米电视大师 82英寸</div>-->
-<!--                    <div class="pro-price">8999元</div>-->
-<!--                  </a>-->
-<!--                </li>-->
-<!--                <li class="product">-->
-<!--                  <a href="javascript:void(0)" target="_blank">-->
-<!--                    <div class="pro-img">-->
-<!--                      <img-->
-<!--                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"-->
-<!--                        alt="小米10至尊纪念版" />-->
-<!--                    </div>-->
-<!--                    <div class="pro-name">小米电视大师 82英寸</div>-->
-<!--                    <div class="pro-price">8999元</div>-->
-<!--                  </a>-->
-<!--                </li>-->
-<!--                <li class="product">-->
-<!--                  <a href="javascript:void(0)" target="_blank">-->
-<!--                    <div class="pro-img">-->
-<!--                      <img-->
-<!--                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"-->
-<!--                        alt="小米10至尊纪念版" />-->
-<!--                    </div>-->
-<!--                    <div class="pro-name">小米电视大师 82英寸</div>-->
-<!--                    <div class="pro-price">8999元</div>-->
-<!--                  </a>-->
-<!--                </li>-->
-<!--                <li class="product">-->
-<!--                  <a href="javascript:void(0)" target="_blank">-->
-<!--                    <div class="pro-img">-->
-<!--                      <img-->
-<!--                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"-->
-<!--                        alt="小米10至尊纪念版" />-->
-<!--                    </div>-->
-<!--                    <div class="pro-name">小米电视大师 82英寸</div>-->
-<!--                    <div class="pro-price">8999元</div>-->
-<!--                  </a>-->
-<!--                </li>-->
-<!--                <li class="product">-->
-<!--                  <a href="javascript:void(0)" target="_blank">-->
-<!--                    <div class="pro-img">-->
-<!--                      <img-->
-<!--                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"-->
-<!--                        alt="小米10至尊纪念版" />-->
-<!--                    </div>-->
-<!--                    <div class="pro-name">小米电视大师 82英寸</div>-->
-<!--                    <div class="pro-price">8999元</div>-->
-<!--                  </a>-->
-<!--                </li>-->
-<!--                <li class="product">-->
-<!--                  <a href="javascript:void(0)" target="_blank">-->
-<!--                    <div class="pro-img">-->
-<!--                      <img-->
-<!--                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"-->
-<!--                        alt="小米10至尊纪念版" />-->
-<!--                    </div>-->
-<!--                    <div class="pro-name">小米电视大师 82英寸</div>-->
-<!--                    <div class="pro-price">8999元</div>-->
-<!--                  </a>-->
-<!--                </li>-->
-<!--              </ul>-->
-<!--            </div>-->
-<!--          </div>-->
+          <div class="item-menu">
+            <span>电视</span>
+            <div class="children">
+              <ul>
+                <li class="product">
+                  <a href="javascript:void(0)" target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"
+                        alt="小米10至尊纪念版" />
+                    </div>
+                    <div class="pro-name">小米电视大师 82英寸</div>
+                    <div class="pro-price">8999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="javascript:void(0)" target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"
+                        alt="小米10至尊纪念版" />
+                    </div>
+                    <div class="pro-name">小米电视大师 82英寸</div>
+                    <div class="pro-price">8999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="javascript:void(0)" target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"
+                        alt="小米10至尊纪念版" />
+                    </div>
+                    <div class="pro-name">小米电视大师 82英寸</div>
+                    <div class="pro-price">8999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="javascript:void(0)" target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"
+                        alt="小米10至尊纪念版" />
+                    </div>
+                    <div class="pro-name">小米电视大师 82英寸</div>
+                    <div class="pro-price">8999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="javascript:void(0)" target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"
+                        alt="小米10至尊纪念版" />
+                    </div>
+                    <div class="pro-name">小米电视大师 82英寸</div>
+                    <div class="pro-price">8999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="javascript:void(0)" target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e513ac03d0ebb594bf42f7d6b1b44944.png?thumb=1&w=320&h=220&f=webp&q=90"
+                        alt="小米10至尊纪念版" />
+                    </div>
+                    <div class="pro-name">小米电视大师 82英寸</div>
+                    <div class="pro-price">8999元</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div class="header-search">
           <div class="wrapper">
@@ -247,9 +247,6 @@
           display: inline-block;
           color: #B0B0B0;
           margin-right: 17px;
-          &:last-child {
-            margin-right: 0;
-          }
         }
         .my-cart {
           width: 110px;
@@ -271,7 +268,6 @@
       .container {
         @include flex();
         height: 112px;
-        line-height: 112px;
         position: relative;
         .header-logo {
           display: inline-block;
@@ -298,71 +294,77 @@
           }
         }
         .header-menu {
-          display: inline-block;
-          padding-left: 209px;
-          flex: 1;
+          display:inline-block;
+          padding-left:209px;
+          width:643px;
           .item-menu {
-            display: inline-block;
-            width: 100px;
-            color: $colorB;
-            font-size: $fontI;
-            font-weight: bold;
-            margin-right: 20px;
+            display:inline-block;
+            line-height:112px;
+            color:$colorB;
+            font-size:$fontI;
+            font-weight:bold;
+            margin-right:20px;
             span {
-              cursor: pointer;
+              cursor:pointer;
             }
             &:hover {
-              color: $colorA;
+              color:$colorA;
               .children {
-                height: 220px;
-                opacity: 1;
-                transition: all .5s;
+                height:220px;
+                border-top:1px solid #E5E5E5;
+                opacity:1;
               }
             }
             .children {
-              position: absolute;
-              top: 112px;
-              left: 0;
-              width: 1226px;
-              height: 0;
-              opacity: 0;
-              border-top: 1px solid #E5E5E5;
-              box-shadow: 0 7px 6px 0 rgba(0,0,0,.11);
-              z-index: 10;
-              transition: all .5s;
+              position:absolute;
+              top:112px;
+              left:0;
+              width:1226px;
+              height:0;
+              opacity:0;
+              box-shadow:0 7px 6px 0 rgba(0,0,0,.11);
+              z-index:10;
+              transition:all .5s;
+              background-color:#ffffff;
               .product {
-                float: left;
-                width: 16.66%;
-                height: 220px;
-                font-size: 12px;
-                line-height: 12px;
-                text-align: center;
+                position:relative;
+                float:left;
+                width:16.6%;
+                height:220px;
+                font-size:12px;
+                line-height:12px;
+                text-align:center;
                 a {
-                  display: inline-block;
+                  display:inline-block;
+                }
+                img {
+                  width:auto;
+                  height:111px;
+                  margin-top:26px;
                 }
                 .pro-img {
-                  height: 137px;
-                  img {
-                    width: auto;
-                    height: 111px;
-                    margin-top: 26px;
-                    border-right: 1px solid $colorF;
-                  }
+                  height:137px;
                 }
                 .pro-name {
-                  color: $colorB;
-                  font-weight: bold;
-                  margin: 19px 0 8px 0;
+                  color:$colorB;
+                  font-weight:bold;
+                  margin-top:19px;
+                  margin-bottom:8px;
                 }
                 .pro-price {
-                  color: $colorA;
+                  color:$colorA;
                 }
-                &:last-child {
-                  .pro-img {
-                    img {
-                      border-right: none;
-                    }
-                  }
+                &:before{
+                  content:' ';
+                  position:absolute;
+                  top:28px;
+                  right:0;
+                  border-left:1px solid $colorF;
+                  height:100px;
+                  width:1px;
+                }
+                &:last-child:before{
+                  display:none;
                 }
               }
             }
@@ -390,7 +392,6 @@
             }
           }
         }
-
       }
     }
   }
